556fcbd57c4f206de7bc1225748469e8c50de56e
Vlad <sitnikovhm@gmail.com>
2022-04-08 17:08:06 +0000

    Stable

--- filestorage.sh
+++ filestorage.sh
@@ -6,7 +6,7 @@ yum install -y pcre-devel
 yum install -y openssl-devel
 yum install -y perl-ExtUtils-Embed
 yum install -y perl-Digest-MD5
-
+wait
 
 mkdir -p /opt/data1/passport-scans /opt/archive /opt/nginx/php /opt/data1/passport-scans  /opt/data1/photos /opt/data1/signed-docs /opt/data1/nginx/ /opt/data1/nginx/logs  /opt/data1/nginx/php /opt/data1/nginx/src /opt/nginx/sbin /opt/nginx/ /opt/nginx/src /opt/data/passport-scans /opt/data/photos /opt/data/signed-docs
 
@@ -15,21 +15,24 @@ wget https://nginx.org/download/nginx-1.21.6.tar.gz
 tar zxvf nginx-1.21.6.tar.gz
 cd nginx-1.21.6
 ./configure --sbin-path=/usr/sbin --conf-path=/etc/nginx/nginx.conf --error-log-path=/opt/nginx/logs/error.log --http-log-path=/opt/nginx/logs/access.log --pid-path=/opt/nginx/nginx.pid --user=nginx --group=nginx --with-http_stub_status_module --with-http_ssl_module --with-http_dav_module --without-mail_pop3_module --without-mail_imap_module --with-http_perl_module --with-cc-opt='-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector-strong --param=ssp-buffer-size=4 -grecord-gcc-switches -m64 -mtune=generic -fPIC' --with-ld-opt='-Wl,-z,relro -Wl,-z,now -pie'
+wait
 if [ $? -eq 0 ]; then
 	echo 'OK'
 else
 	echo 'FALSE'
 fi
 
-make && make install
+make 
 if [ $? -eq 0 ]; then
 	echo 'OK'
 else
 	echo 'FALSE'
 fi
-
+wait
+make install
 cd /tmp
 git clone https://github.com/sitnikovhm/jun.git
+wait
 cd jun
 cp clear_old_scans.sh /opt/nginx/php
 cp webdav-extensions.php /opt/nginx/sbin
